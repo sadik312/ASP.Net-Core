5fdcf85aeb8e20efc1c70a33a9b04cacabf63b76
sadik312 <sadikislam46@gmail.com>
2024-01-22 20:29:55 +0000

    commented SignalR hub class

--- SignalRChat/Hubs/ChatHub.cs
+++ SignalRChat/Hubs/ChatHub.cs
@@ -2,9 +2,12 @@ using Microsoft.AspNetCore.SignalR;
 
 namespace SignalRChat.Hubs
 {
-    public class ChatHub : Hub // Hub is builtin class from SignalR
-    {
+    // Hub is builtin class from SignalR
+    // hub class manages connections, groups, messaging
+    public class ChatHub : Hub     {
         public async Task SendMessage(string user, string message)
+        // sendMessage method invoked by client to send msg to all clients
+        // SignalR is asynchronous to provide max stability
         {
             await Clients.All.SendAsync("ReceiveMessage", user, message);
         }
