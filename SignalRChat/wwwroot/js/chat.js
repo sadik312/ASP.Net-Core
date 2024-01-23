eff7ef272d2bf9c6b4e2a8948b1a31d8dae976d9
sadik312 <sadikislam46@gmail.com>
2024-01-23 21:47:24 +0000

    created wwwroot/js/chat.js

---
+++ SignalRChat/wwwroot/js/chat.js
@@ -0,0 +1,12 @@
+"use strict";
+
+var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
+
+// disable send button until connection is established
+document.getElementById("sendButton").disabled = true;
+
+connection.on("ReceiveMessage", function (user, message) {
+    var li = document.createElement("li");
+    document.getElementById("messagesList").appendChild(li);
+    li.textContent = `${user} says ${message}`;
+});
