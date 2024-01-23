3e8ba78e81750d5bfe8e877e1bc56423e246a3c7
sadik312 <sadikislam46@gmail.com>
2024-01-23 22:15:03 +0000

    added comments to chat.js for personal understanding

--- SignalRChat/wwwroot/js/chat.js
+++ SignalRChat/wwwroot/js/chat.js
@@ -1,22 +1,30 @@
 "use strict";
 
+// Creating connection to hub
 var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
 
 // disable send button until connection is established
 document.getElementById("sendButton").disabled = true;
 
+// Client side event handler for ReceiveMessage event
+// when client receives msg from server, func executed
 connection.on("ReceiveMessage", function (user, message) {
     var li = document.createElement("li");
     document.getElementById("messagesList").appendChild(li);
     li.textContent = `${user} says ${message}`;
 }); 
 
+// Start method to initiate SignalR connection
+// after successful connection, enables button 
 connection.start().then(function () {
     document.getElementById("sendButton").disabled = false;
 }).catch(function (err) {
     return console.error(err.toString());
 });
 
+// Add event listener to button
+// when button clicked, retrieves values from userInput & messageInput
+// then invokes server side method sendMessage on SignalR connection with provided values
 document.getElementById("sendButton").addEventListener("click", function (event) {
     var user = document.getElementById("userInput").value;
     var message = document.getElementById("messageInput").value;
